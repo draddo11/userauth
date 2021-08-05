180bfd97f07bb3fe716a6c443c9a1a818048cda3
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-05 16:04:21 +0000

    service implemetation created

--- src/main/java/com/nana/userservice/service/UserServiceImplemetation.java
+++ src/main/java/com/nana/userservice/service/UserServiceImplemetation.java
@@ -1,4 +1,47 @@
 package com.nana.userservice.service;
 
-public class UserServiceImplemetation {
+import com.nana.userservice.models.Role;
+import com.nana.userservice.models.User;
+import com.nana.userservice.repo.RoleRepo;
+import com.nana.userservice.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Service @RequiredArgsConstructor @Transactional @Slf4j
+public class UserServiceImplemetation implements UserService{
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return  userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
 }
