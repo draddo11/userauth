45340ea4205d1c42cd4fd7e2e604f1b3ecb1e1a1
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-05 23:21:19 +0000

    added javx inports to solve the id issues

--- src/main/java/com/nana/userservice/models/Role.java
+++ src/main/java/com/nana/userservice/models/Role.java
@@ -3,18 +3,23 @@ package com.nana.userservice.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-
+//import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+
+import static javax.persistence.GenerationType.AUTO;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Role {
+
     @Id
     @GeneratedValue(strategy= AUTO)
     private Long id;
     private String name;
+
+
 }
--- src/main/java/com/nana/userservice/models/User.java
+++ src/main/java/com/nana/userservice/models/User.java
@@ -3,21 +3,26 @@ package com.nana.userservice.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-
+//import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static javax.persistence.FetchType.*;
+import static javax.persistence.GenerationType.*;
+
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class User {
-    @Id @GeneratedValue(strategy= AUTO)
+
+    @Id @GeneratedValue(strategy=AUTO)
     private Long id;
     private String name;
     private String username;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = EAGER)
     private Collection<Role> roles = new ArrayList<>();
+
 
 }
