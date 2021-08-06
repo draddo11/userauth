bb129c274a33a6e17d6bd9f9d8d7693cff8bb785
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-06 19:55:50 +0000

    updated userresource

--- src/main/java/com/nana/userservice/api/UserResource.java
+++ src/main/java/com/nana/userservice/api/UserResource.java
@@ -37,17 +37,16 @@ public class UserResource {
 
     @PostMapping("/role/addtouser")
     public ResponseEntity <?>addRoleToUser (@RequestBody RoleToUserForm form){
-        userService.addRoleToUser(form.getUsername(),form.getRoleName();
+        userService.addRoleToUser(form.getUsername(),form.getRoleName());
         return ResponseEntity.ok().build();
     }
 
-    @Data
-    class RoleToUserForm{
-        private String username;
-        private String roleName;
+}
 
-
-    }
+@Data
+class RoleToUserForm{
+    private String username;
+    private String roleName;
 
 
 }
