7dc0c420987a9842ffc533f8edc0a1c2b03e18fc
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-08 14:53:25 +0000

    user tokens for access andd refresh coded and pass wordencoded don on service implementation

--- src/main/java/com/nana/userservice/service/UserServiceImplementation.java
+++ src/main/java/com/nana/userservice/service/UserServiceImplementation.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -22,6 +23,7 @@ import java.util.List;
 public class UserServiceImplementation implements UserService , UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -41,6 +43,7 @@ public class UserServiceImplementation implements UserService , UserDetailsServi
     @Override
     public User saveUser(User user) {
         log.info("saving new user {} to the database " , user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return  userRepo.save(user);
     }
 
